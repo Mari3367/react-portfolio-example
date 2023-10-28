f3329c3ea68f3ff9562709ea4994ec3d82513057
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-10-28 20:28:20 +0400

    added a new illustration

--- src/components/Illustrations.js
+++ src/components/Illustrations.js
@@ -1,7 +1,14 @@
-import sampleIll from '../assets/sampleIrll.png';
+import seasons from '../assets/seasons.jpg';
 import './illustrations.css';
 
-const illustrationsList = [{img: sampleIll, name: 'Illustration-1', id: 'ill1'}, {img: sampleIll, name: 'Illustration-2', id: 'ill2'}, {img: sampleIll, name: 'Illustration-3', id: 'ill3'},{img: sampleIll, name: 'illustration-4', id: 'ill4'},{img: sampleIll, name: 'Illustration-5', id: 'ill5'},{img: sampleIll, name: 'Illustration-6', id: 'ill6'},];
+const illustrationsList = [
+{img: seasons, name: 'Illustration-1', id: 'ill1'},
+{img: seasons, name: 'Illustration-2', id: 'ill2'},
+{img: seasons, name: 'Illustration-3', id: 'ill3'},
+{img: seasons, name: 'illustration-4', id: 'ill4'},
+{img: seasons, name: 'Illustration-5', id: 'ill5'},
+{img: seasons, name: 'Illustration-6', id: 'ill6'},
+];
 
 const Illustrations = () => {
     return(
